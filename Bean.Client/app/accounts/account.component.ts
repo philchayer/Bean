3541c5a58ba7ec12bb8ce4c735d80c44c5c9c84c
Philippe Chayer <philchayer@gmail.com>
2017-01-24 13:45:42 -0500

    First try of login/logout implementation

--- Bean.Client/app/accounts/account.component.ts
+++ Bean.Client/app/accounts/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
 import { Subscription } from 'rxjs/Subscription';
 
 import { Account } from './account';
@@ -12,7 +13,9 @@ import { AccountService } from './account.service';
 export class AccountComponent implements OnInit {
     message: string;
     account: Account = new Account();
+
     isLoggedIn: boolean = false;
+    token: string;
 
     constructor(private _accountService: AccountService) { }
 
@@ -21,7 +24,32 @@ export class AccountComponent implements OnInit {
     login(): void {
         console.log('account.component.login() begin...');
 
+        this._accountService.login(this.account)
+            .subscribe(response => this.onLogin(response),
+            error => this.message = <any>error);
 
+        console.log('account.component.login() end');
+    }
+
+    private onLogin(response: Response): void{
+        console.log('account.component.onLogin() begin...');
+        console.log(response);
+
+        // this.token = response.data.access_token;
+        this.message = '';
+        this.isLoggedIn = true;
+
+        console.log('account.component.onLogin() end');
+    }
+
+    logout(): void{
+
+        console.log('account.component.logout() begin ...');
+
+        this.account = new Account();
+        this.isLoggedIn = false;
+
+        console.log('account.component.logout() end');
     }
 
     register(): void {
@@ -32,13 +60,14 @@ export class AccountComponent implements OnInit {
             error => this.message = <any>error);
 
         console.log('account.component.register() finished');
-
     }
-
 
     onRegister(account: Account): void {
         console.log('registration successful!');
 
-        
+        this.account = account;
+        this.isLoggedIn = true;
     }
+
+
 }
--- Bean.Client/app/accounts/account.service.ts
+++ Bean.Client/app/accounts/account.service.ts
@@ -11,14 +11,32 @@ import { Account } from './account';
 export class AccountService {
 
     //private _plantApiUrl = 'api/accounts/accounts.json';
-    private API_URL = 'http://localhost:21709/api/Account';
+    private readonly API_URL = 'http://localhost:21709/';
+    private API_ACCOUNT_URL = this.API_URL + 'api/Account';
 
     constructor(private _http: Http) { }
 
     // todo: manage error handling
     private handleError(error: Response) {
+        console.log('account.service.handleError() begin...');
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
+    }
+
+    login(account: Account): Observable<Response> {
+        console.log('account.service.login() begin...');
+
+        let param = new URLSearchParams();
+        param.set('userName', account.email);
+        param.set('password', account.password);
+        param.set('grant_type', 'password');
+
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this._http.post(`${this.API_URL}/Token`, param, options)
+            .do(response => console.log(typeof(response)))
+            .catch(this.handleError);
     }
 
     register(account: Account): Observable<Account> {
@@ -33,7 +51,7 @@ export class AccountService {
 
         console.log(bodyString);
 
-        return this._http.post(`${this.API_URL}/Register`, bodyString, options)
+        return this._http.post(`${this.API_ACCOUNT_URL}/Register`, bodyString, options)
             .do(data => console.log('account.service.register(), data: ' + data))
             .catch(this.handleError);
     }
