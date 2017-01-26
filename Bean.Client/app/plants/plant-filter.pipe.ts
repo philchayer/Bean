3880f4dc638d53ddaffa67224caac6601a28a772
Philippe Chayer <philchayer@gmail.com>
2017-01-26 16:39:19 -0500

    fixed pipe

--- Bean.Client/app/plants/plant-filter.pipe.ts
+++ Bean.Client/app/plants/plant-filter.pipe.ts
@@ -1,21 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { Plant } from './plant';
+import { IPlants } from './plants';
 
 @Pipe({
     name: 'plantFilter'
 })
 export class PlantFilterPipe implements PipeTransform {
 
-    transform(value: Plant[], filterBy: string): Plant[] {
+    transform(value: IPlants[], filterBy: string): IPlants[] {
 
         filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
 
-        return  filterBy ?
-            value.filter((plant: Plant) =>
-                            plant.name.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
-                            plant.quantityOnHand.toString().indexOf(filterBy) !== -1 ||
-                            plant.family.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
-                            plant.latinName.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
+        return filterBy ?
+            value.filter((plant: IPlants) =>
+                plant.name.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+                plant.latinName.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+                plant.family.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+                plant.binder.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
     }
 }
