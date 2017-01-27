af8ceb5fd9f93411e85409ebcc5c7b83a3d80e2a
Philippe Chayer <philchayer@gmail.com>
2017-01-27 09:55:51 -0500

    Reinforce authentication

--- Bean.Client/app/plants/plant-guard.service.ts
+++ Bean.Client/app/plants/plant-guard.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 
+import { AuthenticationService } from '../auth/auth.service';
+
 @Injectable()
 export class PlantDetailGuard implements CanActivate {
 
-    constructor(private _router: Router) { }
+    constructor(private _router: Router,
+                private _auth: AuthenticationService) { }
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
 
+        // verify the plant.id
         let id = +route.url[1].path;
 
         if (isNaN(id) || id < 0) {
@@ -16,7 +20,15 @@ export class PlantDetailGuard implements CanActivate {
             this._router.navigate(['/plants']);
             // abort current navigation
             return false;
-        };
+        }
+
+        // verify the authentication
+        if (!this._auth.isLoggedIn()) {
+            // user isn't logged in so navigate to login
+            this._router.navigate(['/login']);
+            // abort current navigation
+            return false;
+        }
 
         return true;
     }
